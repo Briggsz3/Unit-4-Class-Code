85e5e25eaf5f39d7a671085f239611307d5dc05b
briggsz3 <briggsz@wlhs.wlwv.k12.or.us>
2024-10-22 09:54:02 -0700

    While and Continue

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,31 @@
 '''
-Name:
+Name: Zachary Briggs
 Date: 10/22/24
 Description: Unit 4 Lesson 1 - while loops
 
 '''
 
+# while loops  run while a condition is true
+# we usually don't know how many times, this scenario
+# is when they are most often used
+
+'''
+some_boolean
+
+while some_boolean is true:
+    code to execute
+    update variable (if forgotten -> infinite loop)
+'''
+
+start_number = 10
+while start_number >= 0:
+    print(start_number)
+    start_number = start_number-1
+print('Blastoff!')
+
+while True:
+    user_age = int(input("Enter age or -1 to quit: "))
+    if user_age == -1:
+        break #exits the loop
+    else:
+        continue #return to top of loop
